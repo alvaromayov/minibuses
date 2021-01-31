cd4b7381e8ecdcbb2878cee2abebacc9a7698995
Alvaro Mayo <alvaromayov@gmail.com>
2021-01-31 19:01:28 +0000

    add desktop header component and style

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 import './header.component.scss';
 
 const Header = () => (
-    <div>
-        header
+    <div className="header">
+        <div className="container">
+            <div>
+                <Link to="/" >
+                    <img className="image" src="https://www.global-rent-a-car.com/fileadmin/_processed_/b/9/csm_PVMD_Mercedes_Vito_2019_black_right_faf191d992.png" alt="" />
+                </Link>
+            </div>
+            <div className="links">
+                <Link to="/contactanos" className="link">Contactanos</Link>
+                <Link to="/" className="link">Login</Link>
+            </div>
+        </div>
     </div>
 )
 
