0aa6740b911bc8adf856fe7e8aabe033ece350f0
Alvaro <alvaromayov@gmail.com>
2021-04-22 16:47:18 +0100

    feat: add v2 of what we do and half banner components

--- src/components/halfBanner/HalfBanner.component.jsx
+++ src/components/halfBanner/HalfBanner.component.jsx
@@ -3,13 +3,16 @@ import './HalfBanner.component.scss';
 
 const HalfBanner = (props) => (
     <div className="full-banner">
-        <div className={`half-banner ${props.right ? "half-banner--right" : ""}`}>
-            <p>
-                {props.info}
-            </p>
+        <div className={`half-banner ${props.right ? "half-banner--right" : "half-banner--left"}`}>
+            <div className="half-banner__container">
+                <h3>{props.title}</h3>
+                <p>
+                    {props.info}
+                </p>
+            </div>
         </div>
         <div className="half-banner">
-            <img src={`${props.image}`} alt=""/>
+            <img src={`${props.image}`} alt="" />
         </div>
     </div>
 )
