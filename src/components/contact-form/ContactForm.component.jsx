24aacf261b6bcfac4589399a64584b27724b0f73
Alvaro <alvaromayov@gmail.com>
2021-04-25 21:06:30 +0100

    add mobile styles for what we do component

--- src/components/contact-form/ContactForm.component.jsx
+++ src/components/contact-form/ContactForm.component.jsx
@@ -7,19 +7,34 @@ class ContactForm extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            
+            displayName:'',
+            lastName: '',
+            email: '',
+            message: ''
         }
     }
+    handleChange = (e) => {
+        const {name, value} = e.target;
+        this.setState({[name]: value});
+    }
+    submitForm = (e) => {
+        e.preventDefault();
+        console.log(this.state.displayName)
+        console.log(this.state.lastName)
+    }
+
     render() {
         return (
             <form className="contact-form">
                 <label>Nombre</label>
-                <input type="text" />
+                <input name="displayName" value={this.displayName} onChange={this.handleChange} type="text" required />
                 <label>Apellidos</label>
-                <input type="text" />
+                <input name="lastName" value={this.lastName} onChange={this.handleChange} type="text" required />
+                <label>Correo electronico</label>
+                <input name="email" value={this.email} onChange={this.handleChange} type="email" required />
                 <label>Escribe aqui tu consulta</label>
-                <input type="text" />
-                <Button variant="contained" color="primary">
+                <input name="message" value={this.message} onChange={this.handleChange} type="text" required />
+                <Button onClick={this.submitForm} variant="contained" color="primary">
                     Enviar solicitud
                 </Button>
             </form>
