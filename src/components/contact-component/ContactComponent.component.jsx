b8012cadec59104272d85a69cbd991f95dbd6aab
Alvaro <alvaromayov@gmail.com>
2021-04-25 21:27:38 +0100

    add mobile styles for what we do component and update content

--- src/components/contact-component/ContactComponent.component.jsx
+++ src/components/contact-component/ContactComponent.component.jsx
@@ -10,7 +10,7 @@ const ContactComponent = () => (
                 <div className="content--half">
                     <h1>Contactanos</h1>
                     <p>Te ha quedado alguna duda?</p>
-                    <p>Mandanos un mensaje y alguien de nuestro maravilloso equipo se pondra en contacto contigo. </p>
+                    <p>Mandanos un mensaje y un miembro de nuestro maravilloso equipo se pondra en contacto contigo. </p>
                 </div>
                 <div className="content--half">
                     <ContactForm />
