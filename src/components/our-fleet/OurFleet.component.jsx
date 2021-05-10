57cd92a2cd29eb12029a810fa4ccdf0a0ffc7d4a
AlvaroMayo <alvaromayov@gmail.com>
2021-05-10 17:17:00 +0100

    content: add new fleet vehicles

--- src/components/our-fleet/OurFleet.component.jsx
+++ src/components/our-fleet/OurFleet.component.jsx
@@ -3,6 +3,8 @@ import ImageCard from '../image-card/ImageCard.component';
 import './OurFleet.component.scss';
 import skoda from '../../assets/images/skoda-super-b.png';
 import vito from '../../assets/images/vito.png';
+import microbus from '../../assets/images/microbus.png';
+import minibus from '../../assets/images/minibus.png';
 
 const OurFleet = () => (
     <div className="our-fleet">
@@ -19,6 +21,18 @@ const OurFleet = () => (
                 benefits={['Conductor', 'Agua', 'Nevera', '3 Viajeros', 'Corta distancia', 'Larga distancia', "Protocolo Covid"]}
                 brands={['mercedes', 'skoda']}
                 image={skoda}
+            />
+            <ImageCard
+                title="Microbus"
+                benefits={['Conductor', 'Agua', 'Nevera', '21 Viajeros', 'Corta distancia', 'Larga distancia', "Protocolo Covid"]}
+                brands={['mercedes']}
+                image={microbus}
+            />
+            <ImageCard
+                title="Minibus"
+                benefits={['Conductor', 'Agua', 'Nevera', '40 Viajeros', 'Corta distancia', 'Larga distancia', "Protocolo Covid"]}
+                brands={['mercedes']}
+                image={minibus}
             />
         </div>
     </div>
